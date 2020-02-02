da07d00a5411735b7c41c9295b6dea0233371650
akeamwoods <a19941@hotmail.co.uk>
2020-02-02 12:41:59 +0000

    Updates

---
+++ precache-manifest.179c47871b2e72d2c4134a990f6b57a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83a34bf94c4b9bf5d4341f1e9c82161f",
+    "url": "/FR_ToDo_List/index.html"
+  },
+  {
+    "revision": "ed36be2d3945e18e7593",
+    "url": "/FR_ToDo_List/static/css/main.7a6a44ef.chunk.css"
+  },
+  {
+    "revision": "435e67535992c84194ea",
+    "url": "/FR_ToDo_List/static/js/2.7bd3b86b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/FR_ToDo_List/static/js/2.7bd3b86b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed36be2d3945e18e7593",
+    "url": "/FR_ToDo_List/static/js/main.85aedc5a.chunk.js"
+  },
+  {
+    "revision": "4329280b9fad5f7fc562",
+    "url": "/FR_ToDo_List/static/js/runtime-main.5692ca01.js"
+  }
+]);
